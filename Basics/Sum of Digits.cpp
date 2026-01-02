6c8cd19458c1164a6b6e6262258441e02dc40ad0
Akshat Sachdeva <akshatsachdeva1102@gmail.com>
2026-01-03 00:39:34 +0530

    update

---
+++ Basics/Sum of Digits.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+  int n=0;
+  cin >> "Enter the number:">> n ;
+  for i in range(0,2){
+    int sum=0;
+    sum += i;
+    return 0;
+  }
+  cout << "The sum of the number is:" << sum;
+  return 0;
+}
